682c11a42bec7e9d7301ed669c677b833c638bf5
DESKTOP-VUUUHMD\User <thi.pedroso.soares@gmail.com>
2024-11-21 22:41:41 -0300

    Monitiry Backend:  Criação de Projeto Spring Boot e inicio de codificação.

---
+++ src/main/java/br/com/projeto/minority/MinorityApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.minority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinorityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinorityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/minority/MinorityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.minority;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinorityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
