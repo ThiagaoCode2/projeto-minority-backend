4b520be0bdc0c4570f93cb1de7e629e380763174
DESKTOP-VUUUHMD\User <thi.pedroso.soares@gmail.com>
2024-11-28 22:48:10 -0300

    Classes de Model/DTO

---
+++ src/main/java/br/com/projeto/minority/model/Usuario.java
@@ -0,0 +1,123 @@
+package br.com.projeto.minority.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table( name = "tb_usuario" )
+public class Usuario 
+{
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
+	private Long id;
+	
+	@NotBlank( message = "O campo nao pode ser vazio" )
+	@Size( min = 2, max = 100 )
+	private String nome;
+	
+	@NotBlank
+	@Size( min = 5, max = 100 )
+	private String usuario;
+	
+	@NotBlank
+	@Size( min = 5, max = 100, message = "senha de 5 a 100" )
+	private String senha;
+	
+	@Column( name = "dt_nascimento" )
+	@JsonFormat( pattern ="yyyy-MM-dd" )
+	private LocalDate dataNascimento;
+	
+	@OneToMany( mappedBy = "usuario", cascade = CascadeType.REMOVE )
+	@JsonIgnoreProperties( {"usuario"} )
+	private List<Postagem> minhasPostagens = new ArrayList<>();
+	
+	public Usuario( Long id, String nome, String usuario, String senha, LocalDate dataNascimento ) 
+	{
+		this.id             = id;
+		this.nome           = nome;
+		this.usuario        = usuario;
+		this.senha          = senha;
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public Usuario( )
+	{
+		//Null
+	}
+
+	public Long getId( ) 
+	{
+		return id;
+	}
+
+	public void setId( Long id ) 
+	{
+		this.id = id;
+	}
+
+	public String getNome( ) 
+	{
+		return nome;
+	}
+
+	public void setNome( String nome ) 
+	{
+		this.nome = nome;
+	}
+
+	public String getUsuario( ) 
+	{
+		return usuario;
+	}
+
+	public void setUsuario( String usuario ) 
+	{
+		this.usuario = usuario;
+	}
+
+	public String getSenha( ) 
+	{
+		return senha;
+	}
+
+	public void setSenha( String senha ) 
+	{
+		this.senha = senha;
+	}
+
+	public LocalDate getDataNascimento( ) 
+	{
+		return dataNascimento;
+	}
+
+	public void setDataNascimento( LocalDate dataNascimento ) 
+	{
+		this.dataNascimento = dataNascimento;
+	}
+
+	public List<Postagem> getMinhasPostagens( ) 
+	{
+		return minhasPostagens;
+	}
+
+	public void setMinhasPostagens( List<Postagem> minhasPostagens ) 
+	{
+		this.minhasPostagens = minhasPostagens;
+	}
+}
