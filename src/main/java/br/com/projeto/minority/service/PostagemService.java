c9ef8da81ac1fae7ecbd103cc8d4f968877818ae
DESKTOP-VUUUHMD\User <thi.pedroso.soares@gmail.com>
2024-12-03 22:54:54 -0300

    Inclusão de Classes de Service (Postagens, Tema e Usuário)

---
+++ src/main/java/br/com/projeto/minority/service/PostagemService.java
@@ -0,0 +1,62 @@
+package br.com.projeto.minority.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.projeto.minority.model.Postagem;
+import br.com.projeto.minority.model.Tema;
+import br.com.projeto.minority.repository.PostagemRepository;
+import br.com.projeto.minority.repository.TemaRepository;
+import br.com.projeto.minority.repository.UsuarioRepository;
+
+
+@Service
+public class PostagemService 
+{
+	@Autowired
+	private  PostagemRepository repoPostegem;
+	
+	@Autowired
+	private UsuarioRepository repoUsuario;
+	
+	@Autowired
+	private TemaRepository repoTema;
+	
+	public Optional<?> cadastrarPostagem( Postagem novaPostagem ) 
+	{
+		Optional<Tema> objetoExistente = repoTema.findById( novaPostagem.getTema( ).getId( ) );
+		
+		return repoUsuario.findById( novaPostagem.getUsuario( ).getId( ) ).map( usuarioExistente -> {
+			if( objetoExistente.isPresent( ) ) 
+			{
+				novaPostagem.setUsuario( usuarioExistente       );
+				novaPostagem.setTema   ( objetoExistente.get( ) );
+				
+				return Optional.ofNullable( repoPostegem.save( novaPostagem ) );
+			} 
+			else 
+			{
+				return Optional.empty( );
+			}
+			
+		}).orElseGet( ( ) -> {
+			return Optional.empty( );
+		} );
+	}
+	
+	public Optional<Postagem> alterarPostagem( Postagem postagemParaAlterar ) 
+	{
+		return repoPostegem.findById( postagemParaAlterar.getId( ) ).map( postagemExistente -> {
+			postagemExistente.setTitulo( postagemParaAlterar.getTitulo( ) );
+			postagemExistente.setTexto ( postagemParaAlterar.getTexto( )  );
+			
+			return Optional.ofNullable( repoPostegem.save( postagemExistente ) );
+			
+		} ).orElseGet( ( ) -> {
+			return Optional.empty();
+		} );
+	}
+
+}
