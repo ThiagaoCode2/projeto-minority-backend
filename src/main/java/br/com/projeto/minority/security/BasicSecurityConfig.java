558bcf3215bf4d46fc133120d91be889f5d5e800
DESKTOP-VUUUHMD\User <thi.pedroso.soares@gmail.com>
2025-01-19 22:02:45 -0300

    Ajustes para efetivar crud de postagem

--- src/main/java/br/com/projeto/minority/security/BasicSecurityConfig.java
+++ src/main/java/br/com/projeto/minority/security/BasicSecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableMethodSecurity
@@ -71,6 +73,21 @@ public class BasicSecurityConfig
                 .roles( "USER" )
                 .build( )
         );
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer( ) 
+    {
+        return new WebMvcConfigurer( ) 
+        {
+            @Override
+            public void addCorsMappings( CorsRegistry registry ) 
+            {
+                registry.addMapping( "/**" )
+                        .allowedOrigins( "*" )
+                        .allowedMethods( "GET", "POST", "PUT", "DELETE", "OPTIONS" );
+            }
+        };
     }
 
 }
